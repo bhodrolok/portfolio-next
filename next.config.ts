5a81407b0abd068a4d09b37a7f69bd4cad79a691
bhodrolok <ranadipcj@proton.me>
2026-08-30 23:49:16 +0530

    add config options for project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  reactStrictMode: true,
+  // Enable Statically Typed Links for e2e type safety for internal navigation
+  // plus help catch broken links when editing/compiling project
+  typedRoutes: true
 };
 
 export default nextConfig;
