38a345db67542e1d127e3eac5b81317dd0e7864e
bhodrolok <ranadipcj@proton.me>
2026-08-31 00:04:30 +0530

    add and use jetbrains mono for sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, JetBrains_Mono} from "next/font/google";
+
 import "./globals.css";
 
 // Fonts 
+
+const JetBrainsMono = JetBrains_Mono({
+  variable: "--font-jb-mono",
+  subsets: ["latin"],
+  weight: "400"
+})
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,7 +32,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="en"
       className={ 
-        `${geistSans.variable} 
+        `${JetBrainsMono.variable} 
         ${geistMono.variable} 
         h-full 
         antialiased`}
